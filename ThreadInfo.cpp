d073d1a590bf9cdfd3aefb99212103f91bba7294
reicrof <c.l.forcier@gmail.com>
2018-04-22 17:15:10 -0400

    Fixed the write/save with dequeue

--- ThreadInfo.cpp
+++ ThreadInfo.cpp
@@ -50,95 +50,85 @@ size_t serializedSize( const ThreadInfo& ti )
 
 size_t serialize( const ThreadInfo& ti, char* data )
 {
-   // const size_t serialSize = serializedSize( ti );
-   // (void)serialSize; // Removed unused warning
-   // const size_t tracesCount = ti._traces.ends.size();
+    const size_t serialSize = serializedSize( ti );
+    (void)serialSize; // Removed unused warning
+    const size_t tracesCount = ti._traces.ends.size();
 
-   // size_t i = 0;
-   // // Traces count
-   // memcpy( &data[i], &tracesCount, sizeof( size_t ) );
-   // i += sizeof( size_t );
+    size_t i = 0;
+    // Traces count
+    memcpy( &data[i], &tracesCount, sizeof( size_t ) );
+    i += sizeof( size_t );
 
-   // // Max depth
-   // memcpy( &data[i], &ti._traces.maxDepth, sizeof( hop::TDepth_t ) );
-   // i += sizeof( hop::TDepth_t );
+    // Max depth
+    memcpy( &data[i], &ti._traces.maxDepth, sizeof( hop::TDepth_t ) );
+    i += sizeof( hop::TDepth_t );
 
-   // //ends
-   // memcpy( &data[i], ti._traces.ends.data(), sizeof( hop::TimeStamp ) * tracesCount );
-   // i += sizeof( hop::TimeStamp ) * tracesCount;
+    //ends
+    std::copy(ti._traces.ends.begin(), ti._traces.ends.end(), (hop::TimeStamp*)&data[i] );
+    i += sizeof( hop::TimeStamp ) * tracesCount;
 
-   // // deltas
-   // memcpy( &data[i], ti._traces.deltas.data(), sizeof( hop::TimeDuration ) * tracesCount );
-   // i += sizeof( hop::TimeDuration ) * tracesCount;
+    // deltas
+    std::copy( ti._traces.deltas.begin(), ti._traces.deltas.end(), (hop::TimeDuration*)&data[i] );
+    i += sizeof( hop::TimeDuration ) * tracesCount;
 
-   // // fileNameIds
-   // memcpy( &data[i], ti._traces.fileNameIds.data(), sizeof( hop::TStrPtr_t ) * tracesCount );
-   // i += sizeof( hop::TStrPtr_t ) * tracesCount;
+    // fileNameIds
+    std::copy( ti._traces.fileNameIds.begin(), ti._traces.fileNameIds.end(), (hop::TStrPtr_t*)&data[i]);
+    i += sizeof( hop::TStrPtr_t ) * tracesCount;
 
-   // // fctNameIds
-   // memcpy( &data[i], ti._traces.fctNameIds.data(), sizeof( hop::TStrPtr_t ) * tracesCount );
-   // i += sizeof( hop::TStrPtr_t ) * tracesCount;
+    // fctNameIds
+    std::copy( ti._traces.fctNameIds.begin(), ti._traces.fctNameIds.end(), (hop::TStrPtr_t*)&data[i]);
+    i += sizeof( hop::TStrPtr_t ) * tracesCount;
 
-   // // lineNbs
-   // memcpy( &data[i], ti._traces.lineNbs.data(), sizeof( hop::TLineNb_t ) * tracesCount );
-   // i += sizeof( hop::TLineNb_t ) * tracesCount;
+    // lineNbs
+    std::copy( ti._traces.lineNbs.begin(), ti._traces.lineNbs.end(), (hop::TLineNb_t*) &data[i] );
+    i += sizeof( hop::TLineNb_t ) * tracesCount;
 
-   // // // groups
-   // // memcpy( &data[i], ti.traces.groups.data(), sizeof( hop::TGroup_t ) * tracesCount );
-   // // i += sizeof( hop::TGroup_t ) * tracesCount;
+    // // groups
+    // memcpy( &data[i], ti.traces.groups.data(), sizeof( hop::TGroup_t ) * tracesCount );
+    // i += sizeof( hop::TGroup_t ) * tracesCount;
 
-   // // depths
-   // memcpy( &data[i], ti._traces.depths.data(), sizeof( hop::TDepth_t ) * tracesCount );
-   // i += sizeof( hop::TDepth_t ) * tracesCount;
+    // depths
+    std::copy( ti._traces.depths.begin(), ti._traces.depths.end(), (hop::TDepth_t*)&data[i] );
+    i += sizeof( hop::TDepth_t ) * tracesCount;
 
-   // assert( i == serialSize );
+    assert( i == serialSize );
 
-   // return i;
-return 0;
+    return i;
 }
 
 size_t deserialize( const char* data, ThreadInfo& ti )
 {
-   // size_t i = 0;
-   // const size_t tracesCount = *(size_t*)&data[i];
-   // i += sizeof( size_t );
-   // ti._traces.maxDepth = *(hop::TDepth_t*)&data[i];
-   // i += sizeof( hop::TDepth_t );
+    size_t i = 0;
+    const size_t tracesCount = *(size_t*)&data[i];
+    i += sizeof( size_t );
+    ti._traces.maxDepth = *(hop::TDepth_t*)&data[i];
+    i += sizeof( hop::TDepth_t );
 
-   // // Resize the arrays
-   // ti._traces.ends.resize( tracesCount );
-   // ti._traces.deltas.resize( tracesCount );
-   // ti._traces.fileNameIds.resize( tracesCount );
-   // ti._traces.fctNameIds.resize( tracesCount );
-   // ti._traces.lineNbs.resize( tracesCount );
-   // ti._traces.depths.resize( tracesCount );
+    // ends
+    std::copy((hop::TimeStamp*)&data[i], ((hop::TimeStamp*) &data[i]) + tracesCount, std::back_inserter(ti._traces.ends));
+    i += sizeof( hop::TimeStamp ) * tracesCount;
 
-   // // ends
-   // memcpy( ti._traces.ends.data(), &data[i], sizeof( hop::TimeStamp ) * tracesCount );
-   // i += sizeof( hop::TimeStamp ) * tracesCount;
+    // deltas
+    std::copy((hop::TimeDuration*)&data[i], ((hop::TimeDuration*)&data[i]) + tracesCount, std::back_inserter(ti._traces.deltas));
+    i += sizeof( hop::TimeDuration ) * tracesCount;
 
-   // // deltas
-   // memcpy( ti._traces.deltas.data(), &data[i], sizeof( hop::TimeDuration ) * tracesCount );
-   // i += sizeof( hop::TimeDuration ) * tracesCount;
+    // fileNameIds
+    std::copy((hop::TStrPtr_t*)&data[i], ((hop::TStrPtr_t*)&data[i]) + tracesCount, std::back_inserter(ti._traces.fileNameIds));
+    i += sizeof( hop::TStrPtr_t ) * tracesCount;
 
-   // // fileNameIds
-   // memcpy( ti._traces.fileNameIds.data(), &data[i], sizeof( hop::TStrPtr_t ) * tracesCount );
-   // i += sizeof( hop::TStrPtr_t ) * tracesCount;
+    // fctNameIds
+    std::copy((hop::TStrPtr_t*) &data[i], ((hop::TStrPtr_t*)&data[i]) + tracesCount, std::back_inserter(ti._traces.fctNameIds));
+    i += sizeof( hop::TStrPtr_t ) * tracesCount;
 
-   // // fctNameIds
-   // memcpy( ti._traces.fctNameIds.data(), &data[i], sizeof( hop::TStrPtr_t ) * tracesCount );
-   // i += sizeof( hop::TStrPtr_t ) * tracesCount;
+    // lineNbs
+    std::copy((hop::TLineNb_t*)&data[i], ((hop::TLineNb_t*)&data[i]) + tracesCount, std::back_inserter(ti._traces.lineNbs));
+    i += sizeof( hop::TLineNb_t ) * tracesCount;
 
-   // // lineNbs
-   // memcpy( ti._traces.lineNbs.data(), &data[i], sizeof( hop::TLineNb_t ) * tracesCount );
-   // i += sizeof( hop::TLineNb_t ) * tracesCount;
+    // depths
+    std::copy((hop::TDepth_t*)&data[i], ((hop::TDepth_t*) &data[i]) + tracesCount, std::back_inserter(ti._traces.depths));
+    i += sizeof( hop::TDepth_t ) * tracesCount;
 
-   // // depths
-   // memcpy( ti._traces.depths.data(), &data[i], sizeof( hop::TDepth_t ) * tracesCount );
-   // i += sizeof( hop::TDepth_t ) * tracesCount;
-
-   // return i;
-return 0;
+    return i;
 }
 
 }
