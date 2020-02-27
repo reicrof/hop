7af3c3a02e683e9616d05841bd7613ed817dd735
reicrof <c.l.forcier@gmail.com>
2020-02-26 20:07:52 -0500

    Fixed MacOs grep regex

--- common/platform/Platform_Unix.cpp
+++ common/platform/Platform_Unix.cpp
@@ -8,6 +8,12 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#ifdef __APPLE__
+#define HOP_GREP_CMD "comm="
+#else
+#define HOP_GREP_CMD "cmd"
+#endif
+
 namespace hop
 {
 
@@ -52,7 +58,7 @@ ProcessInfo getProcessInfoFromProcessName( const char* name )
        * grep process from the result. On MacOs the process name also contains the path, we
        * thus have to check for a preceding '/'
        */
-      snprintf( cmd, sizeof( cmd ), "ps -Ao pid,cmd | grep -E '[0-9]+ \\.?\\/\\S+\\/%s'" , name );
+      snprintf( cmd, sizeof( cmd ), "ps -Ao pid," HOP_GREP_CMD " | grep -E '[0-9]+ \\.?(?:\\/\\S+)?/%s$'" , name );
 
       // Get name from PID
       if( FILE* fp = popen( cmd, "r" ) )
