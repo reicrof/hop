aba83e1887019de40f41e0385efe984ad35aee1c
Charles Landry-Forcier <charles.landry-forcier@autodesk.com>
2020-03-24 12:25:07 -0400

    Fixed issue when name of exec is part of path

--- common/platform/Platform_Unix.cpp
+++ common/platform/Platform_Unix.cpp
@@ -63,7 +63,7 @@ ProcessInfo getProcessInfoFromProcessName( const char* name )
        * grep process from the result. On MacOs the process name also contains the path, we
        * thus have to check for a preceding '/'
        */
-      snprintf( cmd, sizeof( cmd ), "ps -Ao pid," HOP_GREP_CMD " | grep -E '[0-9]+ \\.?\\/?\\S*\\/?%s\\b.*'" , name );
+      snprintf( cmd, sizeof( cmd ), "ps -Ao pid," HOP_GREP_CMD " | grep -E '[0-9]+ \\.?\\/?\\S*\\/?%s\\b($| .*)'" , name );
       // Get name from PID
       if( FILE* fp = popen( cmd, "r" ) )
       {
