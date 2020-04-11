d4f95f5d3e70a14a1d351c43dec4aecb5481d0bc
Charles Landry-Forcier <c.l.forcier@gmail.com>
2020-04-11 10:52:16 -0400

    Using the identity for the hasing of the pointer is a tiny bit faster. Adding the shift won't really improve the quality of the hash as the pointers often have the higher bits to 0 anyway. I'll try to experiment more and see what works best

--- tests/HashSet_test.cpp
+++ tests/HashSet_test.cpp
@@ -2,8 +2,11 @@
 #include <common/HashSet.h>
 
 #include <cassert>
+#include <chrono>
+#include <iostream>
 #include <random>
 #include <unordered_set>
+#include <vector>
 
 int main()
 {
@@ -14,19 +17,78 @@ int main()
    hop_hash_set_t hs = hop_hash_set_create();
    std::unordered_set< void* > reference;
 
-   for( size_t i = 0; i < 1000000; ++i )
+   const size_t itCount = 50000000;
+   std::vector< uint64_t > randNums( itCount );
+   for( size_t i = 0; i < itCount; ++i )
    {
-      uint64_t randomValue = uniform_dist( e1 );
-      bool inserted = hop_hash_set_insert( hs, (void*)randomValue );
-      auto referenceInsert = reference.insert( (void*)randomValue );
+      randNums[ i ] = uniform_dist( e1 );
+   }
+
+   for( size_t i = 0; i < itCount; ++i )
+   {
+      bool inserted = hop_hash_set_insert( hs, (void*)randNums[i] );
+      auto referenceInsert = reference.insert( (void*)randNums[i] );
       HOP_TEST_ASSERT( inserted == referenceInsert.second );
 
-      bool insertFailure = hop_hash_set_insert(hs, (void*)randomValue);
+      bool insertFailure = hop_hash_set_insert(hs, (void*)randNums[i]);
       HOP_TEST_ASSERT( !insertFailure );
 
       HOP_TEST_ASSERT( reference.size() == hop_hash_set_count( hs ) );
    }
 
+   // Clear both to bench
    hop_hash_set_clear( hs );
+   reference.clear();
+
+   // Do some benchmarks
+
+   printf( "Benchmarking insertion on an empty (but pre-allocated) hash set\n" );
+   {
+      auto hopStart = std::chrono::high_resolution_clock::now();
+      for( size_t i = 0; i < itCount; ++i )
+      {
+         hop_hash_set_insert( hs, (void*)randNums[i] );
+      }
+      auto hopEnd = std::chrono::high_resolution_clock::now();
+
+      auto refStart = std::chrono::high_resolution_clock::now();
+      for( size_t i = 0; i < itCount; ++i )
+      {
+         reference.insert( (void*)randNums[i] );
+      }
+      auto refEnd = std::chrono::high_resolution_clock::now();
+
+      auto hopDuration =
+          std::chrono::duration_cast<std::chrono::microseconds>( hopEnd - hopStart ).count();
+      auto refDuration =
+          std::chrono::duration_cast<std::chrono::microseconds>( refEnd - refStart ).count();
+      std::cout << "Hop hash set took : " << hopDuration
+                << " us\nstd hash set took : " << refDuration << " us\n";
+   }
+
+   printf( "Benchmarking lookups of exsiting value\n" );
+   {
+      auto hopStart = std::chrono::high_resolution_clock::now();
+      for( size_t i = 0; i < itCount; ++i )
+      {
+         hop_hash_set_insert( hs, (void*)randNums[i] );
+      }
+      auto hopEnd = std::chrono::high_resolution_clock::now();
+
+      auto refStart = std::chrono::high_resolution_clock::now();
+      for( size_t i = 0; i < itCount; ++i )
+      {
+         reference.insert( (void*)randNums[i] );
+      }
+      auto refEnd = std::chrono::high_resolution_clock::now();
+
+      auto hopDuration =
+          std::chrono::duration_cast<std::chrono::microseconds>( hopEnd - hopStart ).count();
+      auto refDuration =
+          std::chrono::duration_cast<std::chrono::microseconds>( refEnd - refStart ).count();
+      std::cout << "Hop hash set took : " << hopDuration
+                << " us\nstd hash set took : " << refDuration << " us\n";
+   }
+
    hop_hash_set_destroy( hs );
 }
